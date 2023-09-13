2ff57807c5aa8a216cb8121d182ac4f49e548a8f
Ethan Scruton <escruton@uoguelph.ca>
2023-09-12 21:00:01 -0400

    Update main.py to always pass

--- tests/main.py
+++ tests/main.py
@@ -1,2 +1,2 @@
-print("Throwing exception...")
-raise Exception("This should cause the testing to fail")
+print("Tests passed! FYI: I will always succeed. To make me fail, raise an exception")
+#raise Exception("This will cause the testing to fail")
