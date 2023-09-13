d61fc3895fe61811afdf2f848b3e50a801bc01e4
bturnert <bturnert@uoguelph.ca>
2023-09-12 23:09:10 -0400

    Addedfunctionality to add all course information into associated strings. Only works on one Course

--- src/app.py
+++ src/app.py
@@ -1,77 +1,223 @@
 def readCourse():
+    # Set Up of Varables for Dictionary
     file1 = open("testCourse.txt", "r")
-    course_code = "none"
-    title_done = 0
+    courseCode = "none"
+
+    titleDone = 0
     title = ""
-    credit_weight = "none"
+
+    offered = ""
+
+    creditWeight = ""
+
     description = ""
     isDescription = 0
+
+    format = ""
+    isFormat = 0
+
     prerequisites = "none"
-    prerequisites_credits = "none"
-    restrictions = ""
-    isRescription = 0
+    isPrerequisites = 0
+    prerequisiteCredits = "none"
+    prevWord = ""
+
     corequisites = "none"
+    isCorequisites = 0
+
+    equates = "none"
+    isEquates = 0
+
+    restrictions = "none"
+    isRestriction = 0
+
     department = "none"
+    isDepartment = 0
+
     location = "none"
+    isLocation = 0
+
     i=0
-    test = 'Restriction(s):'
     semersters = ["Summer", "Summer,", "Fall", "Fall,", "Winter", "Winter,"]
+
+    # Loop Through all Lines in Source File
     for line in file1:
 
+        # Loop Through all Words in Source Line
         for word in line.split():
-            # If Restriction line
-            if word == ('Restriction(s):'):
-                isRescription = 1
-                isDescription = 0
+
+            # ===================== determining what type of section currently on =====================
+            if "(s):" in word:
+                # 0 = not done yet
+                # 1 = starting/in progress
+                # 2 = completed/ommitted
+                if word == "Offering(s):":
+                    isDescription = 2
+                    isFormat = 1
+                    isPrerequisites = 0
+                    isCorequisites = 0
+                    isEquates = 0
+                    isRestriction = 0
+                    isDepartment = 0
+                    isLocation = 0
+                
+                elif word == "Prerequisite(s):":
+                    isDescription = 2
+                    isFormat = 2
+                    isPrerequisites = 1
+                    isCorequisites = 0
+                    isEquates = 0
+                    isRestriction = 0
+                    isDepartment = 0
+                    isLocation = 0
+                    prevWord = word
+
+
+                elif word == "Co-requisite(s):":
+                    isDescription = 2
+                    isFormat = 2
+                    isPrerequisites = 2
+                    isCorequisites = 1
+                    isEquates = 0
+                    isRestriction = 0
+                    isDepartment = 0
+                    isLocation = 0
+
+                elif word == "Equate(s):":
+                    isDescription = 2
+                    isFormat = 2
+                    isPrerequisites = 2
+                    isCorequisites = 2
+                    isEquates = 1
+                    isRestriction = 0
+                    isDepartment = 0
+                    isLocation = 0
+
+                elif word == ('Restriction(s):'):
+                    isDescription = 2
+                    isFormat = 2
+                    isPrerequisites = 2
+                    isCorequisites = 2
+                    isEquates = 2
+                    isRestriction = 1
+                    isDepartment = 0
+                    isLocation = 0
+
+                elif word == "Department(s):":
+                    isDescription = 2
+                    isFormat = 2
+                    isPrerequisites = 2
+                    isCorequisites = 2
+                    isEquates = 2
+                    isRestriction = 2
+                    isDepartment = 1
+                    isLocation = 0
+
+                elif word == "Location(s):":
+                    isDescription = 2
+                    isFormat = 2
+                    isPrerequisites = 2
+                    isCorequisites = 2
+                    isEquates = 2
+                    isRestriction = 2
+                    isDepartment = 2
+                    isLocation = 1
+
+            # ===================== Adding information to variables =====================
             
-            if word == "Prerequisite(s):":
-                isDescription = 0
+            # For only the first and second lines of a course's information
+            # to ensure course code, title, and semester offerings are recorded correctly
+            if (i==0 or i == 1):
+                # Course Code
+                if courseCode == "none": courseCode = word
 
-            if word == "Equate(s):":
-                isDescription = 0
-            
-            if word == "Offering(s):":
-                isDescription = 0
-            
-            if word == "Co-requisite(s):":
-                isDescription = 0
-
-            if word == "Department(s):":
-                isDescription = 0
-
-            if word == "Location(s):":
-                isDescription = 0
-
-            if (isRescription == 1):
-                if "*" in word:
-                    restrictions = restrictions + " " + word
-            #First line of course
-            if (i==0):
-                if (course_code == "none"):
-                    course_code = word
-                if  (title_done == 0):
+                # Course Title
+                if titleDone == 0:
+                    # Check for end of title
                     if word in semersters:
-                        title_done = 1
+                        titleDone = 1
+                    
+                    #adding to title if not name of course 
                     elif "*" not in word:
-                        title = title + " " + word
+                        if title == "": title = word
+                        else: title = title + " " + word
+                
+                # Semester Offerings
+                if word in semersters:
+                    if offered == "": offered = word
+                    else: offered = offered + " " + word
 
+            # For all subsequent lines of a course's information
+            # Description
             if isDescription == 1:
-                description = description + " " + word
+                if description == "": description = word
+                else: description = description + " " + word
                 
             #Check for course weight
             if word[0]=="[":
-                credit_weight = word[1:-1]
-                isDescription = 1
+                creditWeight = word[1:-1]
+                if isDescription != 2: isDescription = 1
 
-            
+            # Skipping classification keyword
+            if "(s):" not in word:
+
+                # Formats
+                if isFormat == 1:
+                    if format == "": format = word
+                    else: format = format + " " + word
+
+                # Prerequisites
+                if isPrerequisites == 1:
+                    if "credits" == word:
+                        if "." in prevWord: prerequisiteCredits = prevWord
+                    elif "." not in word:
+                        if prerequisites == "none": prerequisites = word
+                        else: prerequisites = prerequisites + " " + word
+                    prevWord = word
+
+                # Co-Requisites
+                if isCorequisites == 1:
+                    if corequisites == "none": corequisites = word
+                    else: corequisites = corequisites + " " + word
+
+                # Equates
+                if isEquates == 1:
+                    if equates == "none": equates = word
+                    else: equates = equates + " " + word
+
+                # Restrictions
+                if isRestriction == 1:
+                    if "*" in word:
+                        if restrictions == "none": restrictions = word
+                        else: restrictions = restrictions + " " + word
+
+                # Department
+                if isDepartment == 1:
+                    if department == "none": department = word
+                    else: department = department + " " + word
+
+                # Location
+                if isLocation == 1:
+                    if location == "none": location = word
+                    else: location = location + " " + word
+
+        # iterate line tracker
         i = i + 1
-        isRescription = 0
+
     file1.close  
-    print(course_code)
+    print(courseCode)
     print(title)
+    print(offered)
+    print(creditWeight)
     print(description)
-    print(credit_weight)
+    print(format)
+    print(prerequisites)
+    print(prerequisiteCredits)
+    print(corequisites)
+    print(equates)
     print(restrictions)
+    print(department)
+    print(location)
+    
 # def addToCourses
         
 # def parser(filename)
@@ -87,5 +233,5 @@ readCourse()
 #         for line in file1:
 #             for word in line.split():
 #                 '*' in word
-#                     course_code = word
+#                     courseCode = word
 
