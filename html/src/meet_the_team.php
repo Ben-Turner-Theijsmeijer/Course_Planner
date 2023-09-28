04026adffa7e8c6fe36211b3a431001dad68e171
qmeiszinger <quinn.meiszinger@gmail.com>
2023-09-28 16:02:35 -0400

    Changed colour to Guelph colours

--- html/src/meet_the_team.php
+++ html/src/meet_the_team.php
@@ -10,11 +10,11 @@
 </head>
 <body class="bg-black">
 <!-- Header -->
-<div class="flex flex-col w-screen h-fill bg-hero bg-no-repeat bg-cover bg-top bg-[url('../imgs/background.png')]">
+<div class="flex flex-col w-screen h-full bg-hero bg-no-repeat bg-cover bg-top bg-[url('../imgs/background.png')]">
     <header class="bg-black w-screen">
         <div class="flex flex-row">
             <div class="flex flex-row w-1/2 bg-black h-20 items-center py-6 px-10">
-                <span class="font-sans font-bold text-white text-4xl leading-10 underline decoration-yellow-400 decoration-4 underline-offset-4">Course Selector</span>
+                <span class="font-sans font-bold text-white text-4xl leading-10 underline decoration-[#FFC72A] decoration-4 underline-offset-4">Course Selector</span>
             </div>
             <div class="flex gap-x-6 flex-row-reverse w-1/2 bg-black h-20 items-center py-6 px-10">
                 <?php
@@ -22,7 +22,7 @@
                     <form method="post" action="meet_the_team.php">
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             Meet The Team
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </form>
                     ';
@@ -32,7 +32,7 @@
                     <form method="post" action="about.php">
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             About
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </form>
                     ';
@@ -40,12 +40,12 @@
             </div>
         </div>
     </header>
-    <content class="flex w-fill h-fill flex-col mx-10 my-10 px-10 py-10 bg-hero bg-black bg-opacity-50 bg-no-repeat bg-cover bg-center bg-fixed rounded-md">
-        <span class="font-sans font-bold text-white text-4xl underline decoration-yellow-400 decoration-4 underline-offset-4">Meet The Team</span>
-        <div class="flex w-fill h-fill flex-wrap justify-center">
+    <content class="flex w-fill h-screen flex-col mx-10 my-10 px-10 py-10 bg-hero bg-black bg-opacity-50 bg-no-repeat bg-cover bg-center bg-fixed rounded-md">
+        <span class="font-sans font-bold text-white text-4xl underline decoration-[#FFC72A] decoration-4 underline-offset-4">Meet The Team</span>
+        <div class="flex w-fill h-full flex-wrap justify-center">
 
-            <div class="p-4 max-w-sm">
-                <div class="flex rounded-lg h-full bg-red-500 p-8 flex-col">
+            <div class="p-4 max-w-md max-h-md">
+                <div class="flex rounded-lg h-full bg-[#C20430] bg-opacity-75 p-8 flex-col">
                     <div class="flex items-center mb-3">
                         <h2 class="font-sans font-bold text-white text-2xl">Andrew</h2>
                     </div>
@@ -57,14 +57,14 @@
                         </p>
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             More
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </div>
                 </div>
             </div>
 
-            <div class="p-4 max-w-sm">
-                <div class="flex rounded-lg h-full bg-red-500 p-8 flex-col">
+            <div class="p-4 max-w-md max-h-md">
+                <div class="flex rounded-lg h-full bg-[#C20430] bg-opacity-75 p-8 flex-col">
                     <div class="flex items-center mb-3">
                         <h2 class="font-sans font-bold text-white text-2xl">Ben</h2>
                     </div>
@@ -76,14 +76,14 @@
                         </p>
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             More
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </div>
                 </div>
             </div>
 
-            <div class="p-4 max-w-sm">
-                <div class="flex rounded-lg h-full bg-red-500 p-8 flex-col">
+            <div class="p-4 max-w-md max-h-md">
+                <div class="flex rounded-lg h-full bg-[#C20430] bg-opacity-75 p-8 flex-col">
                     <div class="flex items-center mb-3">
                         <h2 class="font-sans font-bold text-white text-2xl">Darren</h2>
                     </div>
@@ -95,14 +95,14 @@
                         </p>
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             More
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </div>
                 </div>
             </div>
 
-            <div class="p-4 max-w-sm">
-                <div class="flex rounded-lg h-full bg-red-500 p-8 flex-col">
+            <div class="p-4 max-w-md max-h-md">
+                <div class="flex rounded-lg h-full bg-[#C20430] bg-opacity-75 p-8 flex-col">
                     <div class="flex items-center mb-3">
                         <h2 class="font-sans font-bold text-white text-2xl">Ethan</h2>
                     </div>
@@ -114,14 +114,14 @@
                         </p>
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             More
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </div>
                 </div>
             </div>
 
-            <div class="p-4 max-w-sm">
-                <div class="flex rounded-lg h-full bg-red-500 p-8 flex-col">
+            <div class="p-4 max-w-md max-h-md">
+                <div class="flex rounded-lg h-full bg-[#C20430] bg-opacity-75 p-8 flex-col">
                     <div class="flex items-center mb-3">
                         <h2 class="font-sans font-bold text-white text-2xl">Noureldeen</h2>
                     </div>
@@ -133,14 +133,14 @@
                         </p>
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             More
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </div>
                 </div>
             </div>
 
-            <div class="p-4 max-w-sm">
-                <div class="flex rounded-lg h-full bg-red-500 p-8 flex-col">
+            <div class="p-4 max-w-md max-h-md">
+                <div class="flex rounded-lg h-full bg-[#C20430] bg-opacity-75 p-8 flex-col">
                     <div class="flex items-center mb-3">
                         <h2 class="font-sans font-bold text-white text-2xl">Quinn</h2>
                     </div>
@@ -152,14 +152,14 @@
                         </p>
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             More
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </div>
                 </div>
             </div>
 
-            <div class="p-4 max-w-sm">
-                <div class="flex rounded-lg h-full bg-red-500 p-8 flex-col">
+            <div class="p-4 max-w-md max-h-md">
+                <div class="flex rounded-lg h-full bg-[#C20430] bg-opacity-75 p-8 flex-col">
                     <div class="flex items-center mb-3">
                         <h2 class="font-sans font-bold text-white text-2xl">Vrushang</h2>
                     </div>
@@ -171,7 +171,7 @@
                         </p>
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             More
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </div>
                 </div>
