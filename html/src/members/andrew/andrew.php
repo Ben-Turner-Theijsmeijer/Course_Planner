263f11c27c5b892e1bf18d76057aad7758fc3cdd
Andrew Chow <ac.andrewchow@gmail.com>
2023-09-29 01:32:11 -0400

    [SPRINT-3] Moves PHP script to one file

--- html/src/members/andrew/andrew.php
+++ html/src/members/andrew/andrew.php
@@ -46,7 +46,7 @@
         <p class="mt-4">
             Here's my php script where a user can select a currency to convert
         </p>
-        <form action="converter.php" method="post" class="mt-4">
+        <form action="" method="post" class="mt-4">
             <div class="mb-4">
                 <label for="amount" class="block font-sans font-bold text-white">Amount:</label>
                 <input type="text" name="amount" id="amount" class="rounded px-2 py-1 border border-gray-400"
@@ -75,8 +75,56 @@
                     <option value="JPY">JPY - Japanese Yen</option>
                 </select>
             </div>
-            <button type="submit" class="bg-yellow-400 hover:bg-yellow-300 text-black px-8 py-2 mt-4 rounded font-bold">Convert</button>
+            <button type="submit"
+                class="bg-yellow-400 hover:bg-yellow-300 text-black px-8 py-2 mt-4 mb-6 rounded font-bold">Convert</button>
         </form>
+
+        <?php
+
+        // Receive values
+        $amount = floatval($_POST["amount"]);
+        $from_currency = $_POST["from_currency"];
+        $to_currency = $_POST["to_currency"];
+
+        // Stores conversion rates for currencies supported
+        $conversion_rates = [
+            "USD" => [
+                "CAD" => 1.35,
+                "EUR" => 0.95,
+                "GBP" => 0.82,
+                "JPY" => 149.43,
+
+            ],
+            "EUR" => [
+                "CAD" => 1.43,
+                "USD" => 1.06,
+                "GBP" => 0.87,
+                "JPY" => 157.81,
+            ],
+            "CAD" => [
+                "USD" => 0.74,
+                "GBP" => 0.61,
+                "JPY" => 110.63,
+                "EUR" => 0.70,
+            ],
+            "GBP" => [
+                "USD" => 1.22,
+                "CAD" => 1.65,
+                "JPY" => 182.27,
+                "EUR" => 1.15,
+            ]
+        ];
+
+        // Output the converted amount
+        if (isset($conversion_rates[$from_currency]) && isset($conversion_rates[$from_currency][$to_currency])) {
+            $converted_amount = $amount * $conversion_rates[$from_currency][$to_currency];
+            echo "<h1 class='font-sans font-bold text-black'>Converted Amount: {$converted_amount} {$to_currency}</h1>";
+        } else {
+            echo "<h1 class='font-sans font-bold text-black'>Invalid Currency!</h1>";
+        }
+
+
+        ?>
     </div>
 </section>
 
