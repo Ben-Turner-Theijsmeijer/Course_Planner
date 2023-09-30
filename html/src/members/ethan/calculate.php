58675bdf93f8c3f6a8413ed7a70c775d8e2918f7
nnourr <nourqwerty123@gmail.com>
2023-09-29 21:43:36 -0400

    changing yellow

--- html/src/members/ethan/calculate.php
+++ html/src/members/ethan/calculate.php
@@ -1,82 +1,82 @@
 <!DOCTYPE html>
 <html>
-    <!-- Head Section -->
-    <head>
-        <meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-    	<title>Calculate</title>
-    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    	<script src="https://cdn.tailwindcss.com"></script>
-    </head>
+<!-- Head Section -->
 
-    <!-- Header -->
-    <header class="bg-black w-screen">
-        <div class="flex flex-row">
-            <div class="flex flex-row w-1/2 bg-black h-20 items-center py-6 px-10">
-                <span class="font-sans font-bold text-white text-4xl leading-10 underline decoration-yellow-400 decoration-4 underline-offset-4">Calculate</span>
-            </div>
-            <div class="flex gap-x-6 flex-row-reverse w-1/2 bg-black h-20 items-center py-6 px-10">
-                <?php
-                    echo '
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculate</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<!-- Header -->
+<header class="bg-black w-screen">
+    <div class="flex flex-row">
+        <div class="flex flex-row w-1/2 bg-black h-20 items-center py-6 px-10">
+            <span class="font-sans font-bold text-white text-4xl leading-10 underline decoration-[#FFC72A] decoration-4 underline-offset-4">Calculate</span>
+        </div>
+        <div class="flex gap-x-6 flex-row-reverse w-1/2 bg-black h-20 items-center py-6 px-10">
+            <?php
+            echo '
                     <form method="post" action="src/meet_the_team.php">
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             Meet The Team
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </form>
                     ';
-                ?>
-                <?php
-                    echo '
+            ?>
+            <?php
+            echo '
                     <form method="post" action="src/about.php">
                         <button class="group font-sans font-bold text-white text-2xl transition duration-300">
                             About
-                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-yellow-400"></span>
+                            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-[#FFC72A]"></span>
                         </button>
                     </form>
                     ';
+            ?>
+        </div>
+    </div>
+</header>
+
+<body>
+    <section class="content">
+        <div class="flex bg-white shadow-md rounded px-8">
+            <div class="border-solid border-2 mb-4">
+                <?php
+                // get form variables
+                $operator = $_POST['operator'];
+                $operand1 = $_POST['operand1'];
+                $operand2 = $_POST['operand2'];
+
+                if ($operator == 'divide' and $operand2 == 0) {
+                    // check if divide by 0
+                    echo "ERROR: divide by 0";
+                } else {
+                    // switch case
+                    switch ($operator) {
+                        case 'add':
+                            echo $operand1, " + ", $operand2, " = ", $operand1 + $operand2;
+                            break;
+                        case 'subtract':
+                            echo $operand1, " - ", $operand2, " = ", $operand1 - $operand2;
+                            break;
+                        case 'multiply':
+                            echo $operand1, " * ", $operand2, " = ", $operand1 * $operand2;
+                            break;
+                        case 'divide':
+                            echo $operand1, " / ", $operand2, " = ", $operand1 / $operand2;
+                            break;
+                    }
+                }
                 ?>
             </div>
-        </div>
-    </header>
-
-    <body>
-        <section class="content">
-            <div class="flex bg-white shadow-md rounded px-8">
-                <div class="border-solid border-2 mb-4">
-                    <?php
-                        // get form variables
-                        $operator = $_POST['operator'];
-                        $operand1 = $_POST['operand1'];
-                        $operand2 = $_POST['operand2'];
-
-                        if ($operator == 'divide' and $operand2 == 0){
-                            // check if divide by 0
-                            echo "ERROR: divide by 0";
-                        } else{
-                            // switch case
-                            switch ($operator){
-                                case 'add':
-                                    echo $operand1, " + ", $operand2, " = ", $operand1 + $operand2;
-                                    break;
-                                case 'subtract':
-                                    echo $operand1, " - ", $operand2, " = ", $operand1 - $operand2;
-                                    break;
-                                case 'multiply':
-                                    echo $operand1, " * ", $operand2, " = ", $operand1 * $operand2;
-                                    break;
-                                case 'divide':
-                                    echo $operand1, " / ", $operand2, " = ", $operand1 / $operand2;
-                                    break;
-                            }
-                        }
-                    ?>
-                </div>
-                <div class="mb-4">
-                    <p class="bg-yellow-400 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"><a href="ethan.php">Return to Ethan's page</p>
-                </div>
+            <div class="mb-4">
+                <p class="bg-[#FFC72A] hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"><a href="ethan.php">Return to Ethan's page</p>
             </div>
-        </section>
-    </body>
-
+        </div>
+    </section>
+</body>
