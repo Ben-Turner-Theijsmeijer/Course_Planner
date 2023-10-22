fbd97f990ac201ebcafaccb4c9dff71d85bbdba3
nnourr <nourqwerty123@gmail.com>
2023-10-22 00:28:09 -0400

    delete course functionality

--- html/src/model/CourseModel.php
+++ html/src/model/CourseModel.php
@@ -1,9 +1,13 @@
 <?php
-require_once(__DIR__.'/Database.php');
+require_once(__DIR__ . '/Database.php');
 class CourseModel extends Database
 {
     public function getCourse($courseCode)
     {
         return $this->select("SELECT * FROM Courses WHERE CourseCode = ?", ["s", $courseCode]);
+    }
+    public function deleteCourse($courseCode)
+    {
+        return $this->delete("DELETE FROM Courses WHERE CourseCode = ?", ["s", $courseCode]);
     }
 }
