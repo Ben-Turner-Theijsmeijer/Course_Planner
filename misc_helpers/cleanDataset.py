a75909446c6516ac5352e27d341cdbc8dacb685b
Andrew Chow <ac.andrewchow@gmail.com>
2023-10-25 18:31:23 -0400

    [SPRINT-5] Deletes and Removes previous sprint files that are no longer relevant

--- misc_helpers/cleanDataset.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-training_data = requests.get('https://datasets-server.huggingface.co/rows?dataset=dair-ai%2Femotion&config=split&split=train&offset=0')
-
-for row in training_data.json()['rows']:
-    print(row['row'])
