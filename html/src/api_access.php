84691cdc9223bb9cf08593fa029137ef5ea684f5
Andrew Chow <ac.andrewchow@gmail.com>
2023-10-25 21:44:40 -0400

    [SPRINT-5] Moves API Commands to separate JS File

--- html/src/api_access.php
+++ html/src/api_access.php
@@ -11,61 +11,14 @@
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="src/js/scripts.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></sc >
+            <script src="src/js/scripts.js"></script>
+    <script src="./js/api.js"></script>
+
     <link rel="icon" href="../src/favicon/favicon.png" type="image/png" />
 
 </head>
 
-<!-- JQUERY - AJAX SECTION -->
-<script>
-
-    $(document).ready(function () {
-        $("#sendRequest").click(function () {
-            $command = jQuery("#command").val();
-            console.log($('#method').val());
-            console.log('https://cis3760f23-11.socs.uoguelph.ca/api/v1/course/' + $command);
-            ;
-
-            $.ajax({
-                url: 'https://cis3760f23-11.socs.uoguelph.ca/api/v1/course/' + $command,
-                method: $('#method').val(),
-                success: function (stuff) {
-                    //alert(JSON.stringify(stuff));
-                    $('#results').text(JSON.stringify(stuff));
-                }
-            });
-        });
-        $("#StudentRequest").click(function () {
-            $command = jQuery("#command1").val();
-            $grade = jQuery("#grade").val();
-            console.log($('#requestType').val());
-            console.log('https://cis3760f23-11.socs.uoguelph.ca/api/v1/student/' + $command + '/' + $grade);
-
-            $.ajax({
-                url: 'https://cis3760f23-11.socs.uoguelph.ca/api/v1/student/' + $command + '/' + $grade,
-                method: $('#requestType').val(),
-                success: function (stuff) {
-                    //alert(JSON.stringify(stuff));
-                    $('#results').text(JSON.stringify(stuff));
-                }
-            });
-        });
-    });
-
-    $(document).ready(function () {
-        $("#requestType").change(function () {
-            if ($(this).val() === "PUT") {
-                // Show the textbox if "PUT" is selected
-                $("#textbox").show();
-            } else {
-                // Hide the textbox for other options
-                $("#textbox").hide();
-            }
-        });
-    });
-
-</script>
 
 <!-- PHP SECTION -->
 <?php
