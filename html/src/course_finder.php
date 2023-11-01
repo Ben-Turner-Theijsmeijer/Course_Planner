ca994640d39acc43e9a31efaee55caf7d39a01dd
Andrew Chow <ac.andrewchow@gmail.com>
2023-10-31 21:39:38 -0400

    [SPRINT-6] Adds calculate credit feature

--- html/src/course_finder.php
+++ html/src/course_finder.php
@@ -33,28 +33,36 @@ echo generateNav('content');
     <h1 class="text-2xl font-bold mb-4">Course Schedule</h1>
     <form id="course-form" class="flex mb-4">
       <input type="text" id="course-code" placeholder="Enter Course Code (i.e CIS*1300 or CIS1300)"
-        class="w-1/2 p-2 rounded-lg border border-gray-300 mr-4" />
-      <button type="button" id="add-course" class="w-1/2 bg-blue-500 text-white p-2 rounded-lg ml-4">Add
+        class="w-1/3 p-2 rounded-lg border border-gray-300 mr-4" />
+      <button type="button" id="add-course" class="w-1/3 bg-blue-500 text-white p-2 rounded-lg ml-4">Add
         Course</button>
+        <button type="button" id="generate-courses" class="w-1/3 bg-green-500 text-white p-2 rounded-lg ml-4">Generate
+        Courses</button>
     </form>
     <table id="my-courses" class="w-full border-collapse ">
       <thead>
         <tr>
           <th class="px-6 py-3 text-left text-gray-600 border-b-2 border-gray-300">Course Code</th>
           <th class="px-6 py-3 text-left text-gray-600 border-b-2 border-gray-300">Course Name</th>
+          <th class="px-6 py-3 text-left text-gray-600 border-b-2 border-gray-300">Credits</th>
           <th class="px-6 py-3 text-left text-gray-600 border-b-2 border-gray-300">Remove</th>
         </tr>
       </thead>
+      <div class="flex justify-center borderpy-2 border-blue-300" id="credit_container">
+        <h1 class="text-black px-2"> Total Credits: </h1>
+        <p class="text-black px-2" id="credits_completed"></p>
+      </div>
       <tbody>
         <!-- Courses are added here -->
         <tr class="hover:bg-gray-100" data-course-id="course-">
         </tr>
       </tbody>
     </table>
+
   </div>
 
   <!-- Courses a student is able to take -->
-  <div class="container mx-auto p-4 bg-white shadow-lg rounded-lg mt-12">
+  <div class="container mx-auto p-4 bg-white shadow-lg rounded-lg mt-8">
     <h1 class="text-2xl font-bold mb-4">Available Courses</h1>
     <div class="grid grid-cols-3 gap-4">
       <div class="bg-blue-300 p-4 rounded-lg">
