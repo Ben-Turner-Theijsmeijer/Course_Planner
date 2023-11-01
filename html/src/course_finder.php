4bf110258518c5f3508c32773cf8f9fadf01319f
Darren Van Helden <vanheldd@uoguelph.ca>
2023-11-01 15:24:27 -0400

    Add accordion back course_finder.php

--- html/src/course_finder.php
+++ html/src/course_finder.php
@@ -21,15 +21,15 @@
   
   <style>
     .accordion {
-      background-color: #eee;
-      color: #444;
+      /* background-color: #eee; */
+      /* color: #444; */
       cursor: pointer;
-      padding: 18px;
+      /* padding: 18px; */
       width: 100%;
       border: none;
       text-align: left;
       outline: none;
-      font-size: 15px;
+      /* font-size: 15px; */
       transition: 0.4s;
     }
 
@@ -116,21 +116,23 @@ echo generateNav('content');
       <div class="bg-blue-300 p-4 rounded-lg">
         <p class="text-xl font-semibold">CIS*2500</p>
         <p>Intermediate Programming</p>
+        <!-- <button class="accordion w-28 " >More Details</button>
+          <div  class="panel" >
+            <p> offered: Fall, Winter. Credits: 0.5</p>
+          </div> -->
       </div>
     </div>
   </div>
 
-    <!-- Courses with no prerequisites -->
-    <div class="container mx-auto p-4 bg-white shadow-lg rounded-lg mt-8">
-    <h1 class="text-2xl font-bold mb-4">Courses with No Prerequisites</h1>
-    <div class="grid grid-cols-3 gap-4"  id="no-prereq-courses">
-      <div class="bg-blue-300 p-4 rounded-lg">
-        <p class="text-xl font-semibold" id="no-prereq-course-code"></p>
-        <p id="no-prereq-course-name"></p>
+  <!-- Courses that have no prereqs -->
+  <div class="container mx-auto p-4 bg-white shadow-lg rounded-lg mt-8">
+    <button class="accordion text-2xl font-bold mb-4 py-2">Courses with No Prerequisites</button>
+    <div  class="panel" >
+      <div id="no-prereq-courses" class="grid grid-cols-3 gap-4">
+        
       </div>
     </div>
   </div>
-
 
   <script>
     var acc = document.getElementsByClassName("accordion");
