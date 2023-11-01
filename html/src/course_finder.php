929507d0e2cf54d517d637211da79110d3e9e1b9
Darren Van Helden <vanheldd@uoguelph.ca>
2023-11-01 14:30:44 -0400

    update course_finder.php

--- html/src/course_finder.php
+++ html/src/course_finder.php
@@ -17,6 +17,46 @@
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> <!-- Axios -->
   <script src="./js/course-finder.js"></script>
   <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@3/swagger-ui.css" />
+  <!-- Accordion https://www.w3schools.com/howto/howto_js_accordion.asp-->
+  
+  <style>
+    .accordion {
+      background-color: #eee;
+      color: #444;
+      cursor: pointer;
+      padding: 18px;
+      width: 100%;
+      border: none;
+      text-align: left;
+      outline: none;
+      font-size: 15px;
+      transition: 0.4s;
+    }
+
+    .active, .accordion:hover {
+      background-color: #ccc;
+    }
+
+    .accordion:after {
+      content: '\002B';
+      color: #777;
+      font-weight: bold;
+      float: right;
+      margin-left: 5px;
+    }
+
+    .active:after {
+      content: "\2212";
+    }
+
+    .panel {
+      padding: 0 18px;
+      background-color: white;
+      max-height: 0;
+      overflow: hidden;
+      transition: max-height 0.2s ease-out;
+    }
+  </style>
 </head>
 
 <!-- Header -->
@@ -80,7 +120,30 @@ echo generateNav('content');
     </div>
   </div>
 
+  <!-- Courses that have no prereqs -->
+  <button class="accordion text-2xl font-bold mb-4 ">Courses with no prereqs</button>
+  <div  class="panel" >
+    <div id="noPreReqs" class="grid grid-cols-3 gap-4">
+      
+    </div>
+  </div>
 
+  <script>
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+      });
+    }
+  </script>
 </body>
 
 
