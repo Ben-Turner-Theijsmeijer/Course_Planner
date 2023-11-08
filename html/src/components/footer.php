64889c9c349fbe851935bb3940c34874ff2e0b1f
qmeiszinger <quinn.meiszinger@gmail.com>
2023-11-07 21:31:23 -0500

    Removed old html folder

--- html/src/components/footer.php
+++
@@ -1,8 +0,0 @@
-    <!-- Footer Section -->
-    <section class="footer w-full">
-        <footer class="bg-black text-white py-6 text-center">
-            <div class="container mx-auto">
-                <p>&copy; CIS 3760 Group 303</p>
-            </div>
-        </footer>
-    </section>
